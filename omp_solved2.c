18d11a2db0b01cbdec115505246d89e4f61db0e9
xinyang0 <xw886@nyu.edu>
2017-03-18 00:10:23 -0400

    Update omp_solved2.c

--- omp_solved2.c
+++ omp_solved2.c
@@ -30,8 +30,7 @@ float total;
 
   /* do some work */
   total = 0.0;
- #pragma omp for schedule(dynamic,10)\
-//	         reduction(+:total)
+ #pragma omp for schedule(dynamic,10)
    for (i=0; i<1000000; i++) 
         total = total + i*1.0;
    printf ("Thread %d is done! Total= %e\n",tid,total);       
